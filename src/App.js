8bbb0cb95dcc9336aafdb11aa03d8fb2f1486b7f
Instituto Precisa Ser <joyceassis96@gmail.com>
2023-03-21 01:21:00 -0300

    aplicando estilo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Api from './Api';
+import * as S from "./Style"
 
 export default class App extends React.Component {
   render() {
     return (
       <>
+      <S.GlobalStyle/>
       <Api/>
       </>
     )
